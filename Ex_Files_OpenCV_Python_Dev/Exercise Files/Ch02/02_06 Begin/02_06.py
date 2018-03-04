88b58d8b2fd07b309517bc262a7fa458cc4d9b49
mittrayash <mittrayash@gmail.com>
2018-03-04 13:35:51 +0530

    Completed 02_06

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_06 Begin/02_06.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_06 Begin/02_06.py
@@ -2,3 +2,20 @@ import numpy as np
 import cv2
 
 image = cv2.imread("thresh.jpg")
+height, width, ch = image.shape
+cv2.imshow('Original', image)
+
+blur = cv2.GaussianBlur(image, (55, 5), 1)
+cv2.imshow('Gaussian Blur', blur)
+
+kernel = np.ones((5, 5), 'uint8')
+
+er = cv2.erode(image, kernel, iterations=1)
+di = cv2.dilate(image, kernel, iterations=1)
+
+cv2.imshow('Erosion', er)
+cv2.moveWindow('Erosion', 0, height)
+cv2.imshow('Dilation', di)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
