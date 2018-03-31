83859c5fd2444afe59a5ec095428a137ab7514fe
mittrayash <mittrayash@gmail.com>
2018-03-31 15:29:31 +0530

    cv2.cvtColor and np.Concatenate

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_04 Begin/02_04.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_04 Begin/02_04.py
@@ -8,15 +8,15 @@ cv2.moveWindow("Butterfly", 0, 0)
 height, width, channels = img.shape
 
 b, g, r = cv2.split(img)
-rgb_split = np.concatenate([b, g, r], axis=1)
-cv2.imshow('rgb', rgb_split)
-cv2.moveWindow('rgb', 0, 0)
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 h, s, v = cv2.split(hsv)
-hsv_split = np.concatenate([h, s, v], axis=1)
-cv2.imshow('hsv', hsv_split)
-cv2.moveWindow('hsv', 0, height)
+
+bgr_split = np.concatenate((b, g, r), axis=1)
+hsv_split = np.concatenate((h, s, v), axis=1)
+
+cv2.imshow('BGR', bgr_split)
+cv2.imshow('HSV', hsv_split)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
