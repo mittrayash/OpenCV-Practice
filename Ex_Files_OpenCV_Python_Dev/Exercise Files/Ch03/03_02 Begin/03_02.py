6ba15b428bbc18401142e81190ef2bdec77d0000
mittrayash <mittrayash@gmail.com>
2018-04-01 00:51:32 +0530

    Thresholding in OpenCV

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_02 Begin/03_02.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_02 Begin/03_02.py
@@ -1,3 +1,13 @@
 import numpy as np
 import cv2
 
+bw = cv2.imread('detect_blob.png', 0)
+cv2.imshow('Original', bw)
+
+threshold = 85
+
+ret, thresh = cv2.threshold(bw, threshold, 255, cv2.THRESH_BINARY)
+cv2.imshow('Threshold applied', thresh)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_03 Begin/03_03.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_03 Begin/03_03.py
@@ -1,2 +1,17 @@
 import numpy as np
 import cv2
+
+original = cv2.imread('sudoku.png', 0)
+cv2.imshow('Original', original)
+
+_, thresh_binary = cv2.threshold(original, 70, 255, cv2.THRESH_BINARY)
+cv2.imshow('Binary Threshold', thresh_binary)
+
+# Note that the block size (301 here) must be an odd number!!!
+thresh_adaptive = cv2.adaptiveThreshold(original, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 301, 1)
+
+
+cv2.imshow('Adaptive Threshold', thresh_adaptive)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
